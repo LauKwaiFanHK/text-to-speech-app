2239d42c39ead95c8189c78398c40f932fe69f23
LauKwaiFanHK <laukwaifanthbde@gmail.com>
2024-01-06 14:54:53 +0100

    Re-organize files structure.

--- basicApp/main.js
+++ basicApp/main.js
@@ -1,12 +1,16 @@
+// Get a reference to the SpeechSynthesis controller.
 const speechService = window.speechSynthesis;
 
+// Access DOM elements.
 const voiceSelector = document.querySelector('#select-voice');
 const inputForm = document.querySelector('form');
 const inputText = document.querySelector('#text-area');
 const submitButton = document.querySelector('#submit-text-button')
 
+// Initiate a variable for voices.
 let myVoices;
 
+// Populate a select menu with voice options so that users can choose a voice.
 const populateVoices = (voices) => {
     myVoices = voices;
     voices.forEach((voice) => {
@@ -27,6 +31,7 @@ const populateVoices = (voices) => {
     });
 };
 
+// Retrieve a list of available voices.
 const loadVoices = () => {
     myVoices = new Promise((resolve, reject) => {
         /**  
@@ -43,17 +48,18 @@ const loadVoices = () => {
 
     myVoices.then((voices) => {
         populateVoices(voices);
-        console.log(voices.json());
     });
 };
 
 loadVoices();
 
 const createSpeech = () => {
+    // Stop handling clicking on submit button when it is speaking.
     if (speechService.speaking) return;
 
     const text = inputText.value;
     if (text !== '') {
+        // Create a new SPeechSynthesisUtterance instance containing input text.
         const utterance = new SpeechSynthesisUtterance(text);
 
         // Inform user when error is thrown.
@@ -62,21 +68,26 @@ const createSpeech = () => {
             console.error(error);
         }
 
+        // Set the utterance's voice to the voice selected from the select menu. 
         const selectedVoiceName = voiceSelector.selectedOptions[0].getAttribute("data-name");
-
         const selectedVoice = myVoices?.find((voice) => voice.name === selectedVoiceName);
-        
         utterance.voice = selectedVoice;
+
+        // Set default pitch and speech speed.
         utterance.pitch = 1;
         utterance.rate = 1;
 
+        // Start speaking.
         speechService.speak(utterance);
+    } else {
+        alert("Please enter text.");
     }
 };
 
 inputForm.onsubmit = (e) => {
+    // Stop the form submitting.
     e.preventDefault();
     createSpeech();
 };
-  
+
 voiceSelector.onchange = (createSpeech);
---
+++ basicApp/readme.md
@@ -0,0 +1,22 @@
+# text-to-speech app
+
+## index.html
+- contains HTML code for the layout and style for the app
+- charset attribute: UTF-8
+    - recommended in HTML5
+    - covers almost all of the characters and symbols
+- viewport: area of the window in which web content can be seen
+    - `device-width`: 100vw (i.e. 100% of the viewport width)
+    - `initial-scale`: used default `1`
+
+## main.js
+- contains JavaScript code to 
+    - render text area input field, dropdown menu and voice options, a Submit text button
+    - handle clicking Submit text button
+    - handle changing voice option
+    - use [Web Speech API](https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API) [SpeechSynthesis](https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis) to utter the submitted text.
+
+## features
+- User can enter text, click on Submit text button to start uttering the text.
+- User can change the voice option.
+- When text is entered, changing voice option automatically re-utters the text.
