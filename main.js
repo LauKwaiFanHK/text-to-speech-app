b86fdd892649d4225f545c3b73a3abd19a962d11
LauKwaiFanHK <laukwaifanthbde@gmail.com>
2024-01-06 13:43:40 +0100

    Add javascript to utter text

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
-const voiceSelector = document.querySelector("#select-voice");
+const speechService = window.speechSynthesis;
 
-const inputForm = document.querySelector("input-form");
+const voiceSelector = document.querySelector('#select-voice');
+const inputForm = document.querySelector('form');
+const inputText = document.querySelector('#text-area');
+const submitButton = document.querySelector('#submit-text-button')
+
+let myVoices;
 
 const populateVoices = (voices) => {
+    myVoices = voices;
     voices.forEach((voice) => {
         const voiceOption = document.createElement('option');
 
-        voiceOption.textContent = `${voice.name} (${voice.lang})`;
+        const voiceLabel = `${voice.name} (${voice.lang})`;
+
+        voiceOption.textContent = voiceLabel;
+
+        if (voice.default) {
+            voiceOption.textContent = `${voiceLabel} - default`;
+        };
+
+        voiceOption.setAttribute("data-lang", voice.lang);
+        voiceOption.setAttribute("data-name", voice.name);
 
         voiceSelector.appendChild(voiceOption);
-    })
+    });
 };
 
 const getVoices = () => {
-    const myVoices = new Promise((resolve, reject) => {
-        const speechService = window.speechSynthesis;
-        
+    myVoices = new Promise((resolve, reject) => {
+        /**  
+         * When a page is loaded, it takes time to populate the voices array as it is done asynchronously.
+         * Fetch voices 0.01 second after initial render to prevent empty voices array.
+        */
         setTimeout(() => {
             const voices = speechService.getVoices();
             if (voices.length > 0) {
@@ -26,21 +43,37 @@ const getVoices = () => {
 
     myVoices.then((voices) => {
         populateVoices(voices);
+        console.log(voices.json());
     });
 };
 
 getVoices();
 
-inputForm.onsubmit = (event) => {
-    event.preventDefault();
-(input)
-    const textInput = inputForm.value;
-    if (textInput !== '') {
-        const utterance = new SpeechSynthesisUtterance(textInput);
+const createSpeech = () => {
+    const text = inputText.value;
+    if (text !== '') {
+        const utterance = new SpeechSynthesisUtterance(text);
 
-        utterance.onerror = (event) => {
+        // Inform user when error is thrown.
+        utterance.onerror = (error) => {
             alert("Oops! Something went wrong!");
+            console.error(error);
         }
+
+        const selectedVoiceName = voiceSelector[0].getAttribute("data-name");
+
+        const selectedVoice = myVoices?.find((voice) => voice.name === selectedVoiceName);
+        
+        utterance.voice = selectedVoice;
+        utterance.pitch = 1;
+        utterance.rate = 1;
+
+        speechService.speak(utterance);
     }
-    
-}
+};
+
+inputForm.onsubmit = (e) => {
+    e.preventDefault();
+    createSpeech();
+};
+  
